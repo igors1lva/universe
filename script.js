f75aaf612a57701c0d273accf204cd88e79686d8
igors1lva <igorfernandosilva3@gmail.com>
2026-09-08 20:27:12 -0300

    Fix nomes com hifens names

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const nome = document.querySelector("#nome").value;
 const genero = document.querySelector("#genero").value;
 const audio = document.querySelector("#audio").value;
 const filme = document.querySelector("#filme").value;
-const filme-serie = document.querySelector("#filme-serie").value;
-const canais-app = document.querySelector("#canais-app").value;
+const filmeserie = document.querySelector("#filmeserie").value;
+const canaisapp = document.querySelector("#canaisapp").value;
 
-resultado.textContent = `Obrigado, ${nome}! Voce ve sempre ${filme-serie}, voce prefere filmes/series de ${genero}. Seu canal/aplicativo favorito é ${canais-app} e seu filme favorito é ${filme}, idioma será ${audio}`;formulario.reset ();
+resultado.textContent = `Obrigado, ${nome}! Voce ve sempre ${filmeserie}, voce prefere filmes/series de ${genero}. Seu canal/aplicativo favorito é ${canaisapp} e seu filme favorito é ${filme}, idioma será ${audio}`;formulario.reset ();
 });
