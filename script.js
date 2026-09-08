56e2e6757a4cbd1236e998dfee4a0feedb8e2543
igors1lva <igorfernandosilva3@gmail.com>
2026-09-08 20:22:22 -0300

    Alterações no javescript
    -Incluído retorno em alguns campos descritivos e selecionaveis do formulário.

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ evento.preventDefault();
 
 const nome = document.querySelector("#nome").value;
 const genero = document.querySelector("#genero").value;
+const audio = document.querySelector("#audio").value;
+const filme = document.querySelector("#filme").value;
+const filme-serie = document.querySelector("#filme-serie").value;
+const canais-app = document.querySelector("#canais-app").value;
 
-resultado.textContent = `Obrigado, ${nome}! Voce prefere filmes de ${genero}.`;formulario.reset ();
+resultado.textContent = `Obrigado, ${nome}! Voce ve sempre ${filme-serie}, voce prefere filmes/series de ${genero}. Seu canal/aplicativo favorito é ${canais-app} e seu filme favorito é ${filme}, idioma será ${audio}`;formulario.reset ();
 });
